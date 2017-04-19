27950017a1da40dd964eb13d2957cf4e2c31caf8
Noel Maersk <veox@users.noreply.github.com>
2017-04-19 16:08:36 +0300

    # This is a combination of 2 commits. # This is the 1st commit message:
    travis: install `solc` dependencies if binary present in cache.
    
    `solc` has many library dependencies that are installed from package
    repositories, but are not cached by Travis between builds. The `solc`
    sources and built binary are cached, though.
    
    Current custom installation script for `solc` skips installing
    the dependencies, which results in broken builds.
    
    # This is the commit message #2:
    
    SQ

--- bin/install_solc.sh
+++ bin/install_solc.sh
@@ -7,6 +7,7 @@ set -e
 set -u
 
 if [ ! -e solc-versions/solidity-0.3.6/build/solc/solc ] ; then
+    # cache not present (first run or cleared), perform full installation procedure
     wget -O solc.tar.gz "https://github.com/ethereum/solidity/archive/v0.3.6.tar.gz"
     install -d solc-versions
     cd solc-versions
@@ -20,5 +21,10 @@ if [ ! -e solc-versions/solidity-0.3.6/build/solc/solc ] ; then
     chmod +x ../../../solc-versions/solc-0.3.6
     echo "Geth installed at $PWD/solc-0.3.6"
 else
+    # cached version present, just install package dependencies
+    cd solc-versions
+    cd solidity-0.3.6
+    ls -al
+    ./scripts/install_deps.sh
     echo "Geth already installed at $PWD/solc/solc-0.3.6"
 fi
