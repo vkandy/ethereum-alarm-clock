db8bae069fb7a25c8973f6697f2f4372c81a55b0
Noel Maersk <veox@users.noreply.github.com>
2017-04-20 21:35:04 +0300

    travis: don't download entire Solidity source - just the deps script.
    Requires Travis cache wipe!

--- bin/install_solc.sh
+++ bin/install_solc.sh
@@ -7,27 +7,29 @@ set -e
 set -u
 
 if [ ! -e $SOLC_BINARY ] ; then
-    echo "Solidity not present, performing full installation procedure..."
+    echo "Solidity not present, fetching binary and dependency installation script."
 
     mkdir -p solc-versions
     cd solc-versions
 
-    # get sources, put in dir source-$SOLC_VERSION
-    git clone --recurse-submodules --branch v$SOLC_VERSION --depth 50 \
-        https://github.com/ethereum/solidity.git source-$SOLC_VERSION
-    ./source-$SOLC_VERSION/scripts/install_deps.sh
+    # get pre-built binaries, put in $SOLC_BINARY_PATH
+    wget "https://github.com/ethereum/solidity/releases/download/v$SOLC_VERSION/solidity-ubuntu-trusty.zip"
+    unzip solidity-ubuntu-trusty.zip -d $SOLC_BINARY_PATH
 
-    # get pre-built binaries, put in dir binary--$SOLC_VERSION
-    wget https://github.com/ethereum/solidity/releases/download/v$SOLC_VERSION/solidity-ubuntu-trusty.zip
-    unzip solidity-ubuntu-trusty.zip -d binary-$SOLC_VERSION
+    echo "Solidity compiler installed as $SOLC_BINARY"
 
-    echo "Solidity installed at $SOLC_BINARY"
-else
-    echo "Solidity already installed at $SOLC_BINARY, installing dependencies..."
+    # get install_deps.sh script
+    cd $SOLC_BINARY_PATH
+    wget "https://github.com/ethereum/solidity/raw/v$SOLC_VERSION/scripts/install_deps.sh"
+    chmod +x install_deps.sh
 
-    cd solc-versions
-    ./source-$SOLC_VERSION/scripts/install_deps.sh
+    # go back
+    cd ../..
 fi
+
+echo "Installing Solidity dependencies..."
+cd solc-versions
+$SOLC_BINARY_PATH/install_deps.sh
 
 # DEBUG
 ls -l $SOLC_BINARY
